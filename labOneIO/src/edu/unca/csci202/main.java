f60dd4fea00e8b95ee4dfa3f4152c9fc5df6deef
Raine <rainehwalker@proton.me>
2026-01-12 10:37:12 -0500

    blech

---
+++ labOneIO/src/edu/unca/csci202/main.java
@@ -0,0 +1,13 @@
+package edu.unca.csci202;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+			for (int i = 0; i < 10; i++) {
+				int input = scanner.nextInt();
+			}
+	}
+
+}
